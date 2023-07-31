905c8016872e96e7b0cc520e26436f2b226a812f
Mel <77106812+melaanieet@users.noreply.github.com>
2023-07-31 16:09:49 +0800

    Update Plot 1.R

--- Plot 1.R
+++ Plot 1.R
@@ -6,7 +6,6 @@ file <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
 
 subdata <- subset(file,file$Date=="1/2/2007" | file$Date =="2/2/2007")
 
-
 #plotting histogram
 
 hist(as.numeric(as.character(subdata$Global_active_power)), xlab = "Global Active Power(kilowatts)", col = "red", main = "Global Active Power")
